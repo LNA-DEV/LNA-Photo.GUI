30fee3a873d7030f4ccff1b322cec399cfbd9cb4
Lukas Nagel <lukas@lna-dev.net>
2022-08-08 12:07:47 -0600

    added data provider

--- lna_photo/lib/DataProviders/photo_in_mem_data_provider.dart
+++ lna_photo/lib/DataProviders/photo_in_mem_data_provider.dart
@@ -1,3 +1,7 @@
 class PhotoInMemDataProvider {
-  
+  List<String> getPhotoUrls() {
+    List<String> photoUrls = ["https://lna-dev.com/Assets/Photo/Mountains%20in%20the%20evening.JPG", "https://lna-dev.com/Assets/Photo/Mountains%20in%20the%20evening.JPG", "https://lna-dev.com/Assets/Photo/Mountains%20in%20the%20evening.JPG"];
+
+    return photoUrls;
+  }
 }
--- lna_photo/lib/Views/gallery_view/gallery_view.dart
+++ lna_photo/lib/Views/gallery_view/gallery_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lna_photo/DataProviders/photo_in_mem_data_provider.dart';
 
 class GalleryView extends StatefulWidget {
   const GalleryView({Key? key}) : super(key: key);
@@ -8,8 +9,21 @@ class GalleryView extends StatefulWidget {
 }
 
 class _GalleryViewState extends State<GalleryView> {
+  PhotoInMemDataProvider dataProvider = PhotoInMemDataProvider();
+
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World");
+    List<String> urls = dataProvider.getPhotoUrls();
+
+    List<Image> images = [];
+
+    for (var element in urls) {
+      Image image = Image.network(element);
+      images.add(image);
+    }
+
+    return ListView(
+      children: images,
+    );
   }
 }
