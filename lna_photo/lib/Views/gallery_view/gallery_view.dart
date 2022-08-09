30d1d065b9dc79911a32af2f6748fc11b100b389
Lukas Nagel <lukas@lna-dev.net>
2022-08-08 22:17:23 -0600

    added future provider

--- lna_photo/lib/Views/gallery_view/gallery_view.dart
+++ lna_photo/lib/Views/gallery_view/gallery_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lna_photo/DataProviders/photo_api_data_provider.dart';
-import 'package:lna_photo/DataProviders/photo_in_mem_data_provider.dart';
+import 'package:lna_photo/Models/photo.dart';
 import 'package:lna_photo/Views/gallery_view/gallery_item.dart';
 
 class GalleryView extends StatefulWidget {
@@ -15,17 +15,51 @@ class _GalleryViewState extends State<GalleryView> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> urls = dataProvider.getPhotoUrls();
+    Future<List<String>> urls = dataProvider.getPhotoUrls();
 
-    List<GalleryItem> galleryItems = [];
+    return FutureBuilder<List<String>>(
+      future: urls,
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        List<Widget> children;
+        if (snapshot.hasData) {
+          List<GalleryItem> galleryItems = [];
 
-    for (String element in urls) {
-      GalleryItem item = GalleryItem(imageSource: element);
-      galleryItems.add(item);
-    }
+          for (String element in snapshot.requireData) {
+            GalleryItem item = GalleryItem(imageSource: element);
+            galleryItems.add(item);
+          }
 
-    return ListView(
-      children: galleryItems,
+          children = galleryItems;
+        } else if (snapshot.hasError) {
+          children = <Widget>[
+            const Icon(
+              Icons.error_outline,
+              color: Colors.red,
+              size: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Error: ${snapshot.error}'),
+            )
+          ];
+        } else {
+          children = const <Widget>[
+            SizedBox(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text('Awaiting result...'),
+            )
+          ];
+        }
+
+        return Column(
+          children: children,
+        );
+      },
     );
   }
 }
