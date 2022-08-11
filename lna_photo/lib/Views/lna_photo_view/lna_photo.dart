1a2e3bc17ff912f2a9ad736693ea660ec3eb19f1
Lukas Nagel <lukas@lna-dev.net>
2022-08-11 18:22:40 +0200

    added text

--- lna_photo/lib/Views/lna_photo_view/lna_photo.dart
+++ lna_photo/lib/Views/lna_photo_view/lna_photo.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 import 'package:lna_photo/Views/gallery_view/gallery_view.dart';
 
 class LnaPhoto extends StatefulWidget {
@@ -12,6 +11,8 @@ class LnaPhoto extends StatefulWidget {
 class _LnaPhotoState extends State<LnaPhoto> {
   @override
   Widget build(BuildContext context) {
-    return const GalleryView();
+    return ListView(
+      children: const [Text("LNA-Photo"), GalleryView()],
+    );
   }
 }
